e74f6f955760528be3c82d2c2b074505430b6e4d
tprincip <tprincip@indiana.edu>
2018-04-22 18:45:07 -0400

    Started validation on Membership sign up

--- S308.FinalProject.Group02/FitnessClub/Membership Signup.xaml.cs
+++ S308.FinalProject.Group02/FitnessClub/Membership Signup.xaml.cs
@@ -33,9 +33,50 @@ namespace FitnessClub
 
         private void btnCreateMember_Click(object sender, RoutedEventArgs e)
         {
+            // do we need this here?
             MainMenu mainmenuWindow = new MainMenu();
             mainmenuWindow.Show();
             this.Close();
+
+            //validate first name - required 
+            if(txtFirstName.Text == "")
+            {
+                MessageBox.Show("Please enter a First Name.");
+                return;
+            }
+
+            //Validate Last Name - required
+            if (txtLastName.Text == "")
+            {
+                MessageBox.Show("Please enter a Last Name.");
+                return;
+            }
+
+            //Validate Email - criteria - required FINISH 
+            if (txtEmail.Text == "")
+            {
+                MessageBox.Show("Please enter a valid email.");
+                return;
+            }
+
+
+            //Phone Number - required
+
+            //validate Credit Card Type - required - cbo
+
+
+            //validate Credit Card Number - required
+
+            //valdate gender - required - cbo
+
+            //validate age 
+
+            //validate weight
+
+            //validate fitness goal - cbo
+
+
+
         }
     }
 }
