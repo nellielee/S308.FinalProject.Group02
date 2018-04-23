bd02ae25ad19b13988df64e38376f516858fd384
tprincip <tprincip@indiana.edu>
2018-04-23 17:00:32 -0400

    Fixed Combo boxed on membership pages. Started Validation  on Credit Card - need to fix code

--- S308.FinalProject.Group02/FitnessClub/Membership Signup.xaml.cs
+++ S308.FinalProject.Group02/FitnessClub/Membership Signup.xaml.cs
@@ -34,9 +34,12 @@ namespace FitnessClub
         private void btnCreateMember_Click(object sender, RoutedEventArgs e)
         {
             // do we need this here?
-            MainMenu mainmenuWindow = new MainMenu();
-            mainmenuWindow.Show();
-            this.Close();
+            //MainMenu mainmenuWindow = new MainMenu();
+            //mainmenuWindow.Show();
+            //this.Close();
+            
+            //create variables
+            string strFirstName
 
             //validate first name - required 
             if(txtFirstName.Text == "")
@@ -59,24 +62,103 @@ namespace FitnessClub
                 return;
             }
 
+            if (!txtEmail.Text.Contains("@") || !txtEmail.Text.Contains("."))
+            {
+                MessageBox.Show("Please enter a valid email.");
+                return;
+            }
 
-            //Phone Number - required
+
+            //Phone Number - required -- ADD
 
             //validate Credit Card Type - required - cbo
 
 
             //validate Credit Card Number - required
+            //1. Declare a variables
+            string strCardNum = txtCreditCardNumber.Text.Trim().Replace(" ", "");
+            long lngOut;        
+            bool bolValid = false;            int i;
+            int intCheckDigit;
+            int intCheckSum = 0;        
+            string strCardType;
+            //2. Make sure the text entered is numeric
+            if (!Int64.TryParse(strCardNum, out lngOut))
+            {
+                MessageBox.Show("Credit card numbers contain only numbers.");
+                return;
+            }
+            //3. Make sure there are 13, 15, 16 digits entered
+            if (strCardNum.Length != 13 && strCardNum.Length != 15 && strCardNum.Length != 16)
+            {
+                MessageBox.Show("Credit card numbers must contain the correct number of digits.");
+                return;
+            }
 
-            //valdate gender - required - cbo
+            //4. Determine the card type from the prefix and set the card type variable
+            if (strCardNum.StartsWith("34") || strCardNum.StartsWith("37"))
+            {
+                strCardType = "AMEX";
+            }
+            else if (strCardNum.StartsWith("6011"))
+            {
+                strCardType = "Discover";
+            }
+            else if (strCardNum.StartsWith("51") || strCardNum.StartsWith("52") || strCardNum.StartsWith("53") || strCardNum.StartsWith("54") || strCardNum.StartsWith("55"))
+            {
+                strCardType = "MasterCard";
+            }
+            else if (strCardNum.StartsWith("4"))
+            {
+                strCardType = "VISA";
+            }            else
+            {
+                strCardType = "Unknown Card Type";
+                MessageBox.Show("Not a Valid Card Number.");
+                 return;
+            }
 
-            //validate age 
 
-            //validate weight
+            //5. Validate card number
+            //ASK THIS SHOULD WORK
+            //strCardNum = ReverseString(strCardNum);
 
-            //validate fitness goal - cbo
+            for (i = 0; i < strCardNum.Length; i++)
+            {
+                intCheckDigit = Convert.ToInt32(strCardNum.Substring(i, 1));
+
+                if ((i + 1) % 2 == 0)
+                {
+                    intCheckDigit *= 2;
+
+                    if (intCheckDigit > 9)
+                    {
+                        intCheckDigit -= 9;
+                    }
+                }
+                intCheckSum += intCheckDigit;            }
+            if (intCheckSum % 10 == 0)
+            {
+                bolValid = true;
+            }
+
+
+            //6. Show the appropriate result          
+
+
+           //valdate gender - required - cbo
+
+
+           //validate age 
 
 
 
-        }
-    }
+           //validate weight
+
+           //validate fitness goal - cbo
+
+
+
+                }
+            }
 }
